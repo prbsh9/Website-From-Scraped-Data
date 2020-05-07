66e8656a82c9b1d9b505866ef45e5432fff3c8ef
Prabesh Gautam <prbsh9@gmail.com>
2020-05-07 17:49:08 +0545

    Made Better

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask
 from flask import render_template
 from nepnews_soup import newsToJson  # this will get news in json format
-# import json
 
 app = Flask(__name__)
 posts = newsToJson()
-# print(posts)
-# jsonFile = open('newsNep.json', encoding='utf8', errors='ignore')
-# posts = json.load(jsonFile)
-
 
 @app.route('/')
 @app.route('/home')
@@ -16,7 +11,5 @@ def index():
     return render_template('tajanews.html', newsHaru=posts)
 
 
-# print(posts)
 if __name__ == '__main__':
-    # app.run(host= '192.168.10.76', debug=True)
     app.run(debug=True)
--- nepnews_soup.py
+++ nepnews_soup.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import json
+import time
 data = []
 
 
@@ -11,35 +12,18 @@ def onlineKhabarNews(x=15):
 
     oKhabar = requests.get('https://www.onlinekhabar.com/').text
     onlineKhabarSoup = BeautifulSoup(oKhabar, 'lxml')
-    # # Main news part
-    # print('Here is the main news of onlinekhabar')
-    print()
 
     mainNews = onlineKhabarSoup.find_all('h2', class_='highlight')
     for mainN in mainNews:
         news1_h = mainN.a.text
-        # print(news1_h)
         news1_a = mainN.a.get('href')
         news1_p = paragraphOnlineKhabar(news1_a)
-        # print(news1_p)
-        # print(news1_a)
         data.append({'source': 'onlinekhabar', 'headline': news1_h, 'summary': news1_p, 'newsLink': news1_a})
-
-    # print()
-    # print()
-
-    # print('Here is the other news of onlinekhabar')
-    # print()
-    # print()
     n = 0
     for oH in onlineKhabarSoup.find_all('h2', class_='post__title'):
-        # print(oH.text)  # oH stands for other headlines
+
         oH_p = paragraphOnlineKhabar(oH.a.get('href'))
-        # print(oH_p)
-        # print(oH.a.get('href'))
         data.append({'source': 'onlinekhabar', 'headline': oH.text, 'summary': oH_p, 'newsLink': oH.a.get('href')})
-        n += 1
-        # print()
         n += 1
         if n > x:  # x is parameter
             break
@@ -50,35 +34,14 @@ def ekantipurNews():
     using BeautifulSoup  '''
     e_kantipur = requests.get('https://www.ekantipur.com/').text
     ekantipurSoup = BeautifulSoup(e_kantipur, 'lxml')
-    # Main news part
-    # print('Here is the main headlines of ekantipur')
-    # print()
 
     for mainN in ekantipurSoup.find_all('h1'):
         if mainN.a is not None:
-            # print(mainN.a.text)
-            # print(mainN.a.get('href'))
-            data.append({'source': 'ekantipur', 'headline': mainN.a.text, 'summary': None, 'newsLink': mainN.a.get('href')})
-            # print()
-            # print()
-    # print()
-    # print()
-    # print()
-    # print('Here are the other headlines of ekantipur')
-    # print()
-
+            data.append({'source': 'ekantipur', 'headline': mainN.a.text, 'summary': '', 'newsLink': mainN.a.get('href')})
     extraNews = ekantipurSoup.find('div', class_='more-main-news').find_all('article')
     for otherN in extraNews:
-        # print(otherN.h2.text)
-        # print()
-        # print(otherN.p.text)
-        # print(otherN.h2.a.get('href'))
         data.append({'source': 'ekantipur', 'headline': otherN.a.text,
              'summary': otherN.p.text, 'newsLink': otherN.a.get('href')})
-        # print()
-        # print()
-
-
 
 
 def paragraphOnlineKhabar(link):
@@ -102,12 +65,12 @@ def newsToJson():
     ekantipurNews()
     with open('newsNep.json', 'w', encoding='utf8') as outfile:
         json.dump(data, outfile, ensure_ascii=False, indent=1)
-    # print(type(data))
-    # print(data)
+    print(type(data))
+    print(data)
     return data
 
 
-
 if __name__ == '__main__':
-    # data = []
-    newsToJson()
+    while True:
+        newsToJson()
+        time.sleep(60*60)
