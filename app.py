363ba32ab13e20d2cf497c502ee0c370e2f80a2d
Prabesh Gautam <prbsh9@gmail.com>
2020-07-24 20:18:00 +0545

    lazy

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import render_template
-from nepnews_soup import newsToJson  # this will get news in json format
+# from nepnews_soup import newsToJson  # this will get news in json format
+import json
 
 app = Flask(__name__)
-posts = newsToJson()
+# posts = newsToJson()
+jsonFile = open('newsNep.json', encoding='utf8', errors='ignore')
+posts = json.load(jsonFile)
 
 @app.route('/')
 @app.route('/home')
@@ -11,5 +14,5 @@ def index():
     return render_template('tajanews.html', newsHaru=posts)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
